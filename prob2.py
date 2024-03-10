cbc52d2655f1d7544201b9db26a8ed7bbb38e945
MrKoBo <kdtate4@memphis.edu>
2024-03-10 18:50:17 -0500

    fixed prob2 by making one lambda function

--- prob2.py
+++ prob2.py
@@ -1,5 +1,5 @@
 words = ["Acedd","Add","Abew", "Aatr"]
-sorted_words = sorted(words)
 
-longest_words = sorted(sorted_words, key=lambda x: len(x))
+longest_words = sorted(words, key=lambda x: (len(x), x))
+
 print(longest_words)
